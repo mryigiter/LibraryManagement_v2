2e8975d4278d61177bd2cd898d99ba5b39f5c630
Yigit Er <mr.yigiter222@gmail.com>
2023-05-04 16:27:00 +0300

    gitignore

--- src/main/java/com/yigiter/librarymanagement/LibraryManagementApplication.java
+++ src/main/java/com/yigiter/librarymanagement/LibraryManagementApplication.java
@@ -1,13 +1,46 @@
 package com.yigiter.librarymanagement;
 
+import com.yigiter.librarymanagement.domain.Role;
+import com.yigiter.librarymanagement.domain.enums.RoleType;
+import com.yigiter.librarymanagement.repository.BorrowerRepository;
+import com.yigiter.librarymanagement.repository.RoleRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.HashSet;
 
 @SpringBootApplication
 public class LibraryManagementApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LibraryManagementApplication.class, args);
+    }
+}
+
+@Component
+@AllArgsConstructor
+class DemoCommandLineRunner implements CommandLineRunner {
+    RoleRepository roleRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        if (!roleRepository.findByType(RoleType.ROLE_MEMBER).isPresent()) {
+            Role roleCustomer = new Role();
+            roleCustomer.setType(RoleType.ROLE_MEMBER);
+            roleRepository.save(roleCustomer);
+        }
+
+        if (!roleRepository.findByType(RoleType.ROLE_ADMIN).isPresent()) {
+            Role roleAdmin = new Role();
+            roleAdmin.setType(RoleType.ROLE_ADMIN);
+            roleRepository.save(roleAdmin);
+        }
     }
 
 }
